dcb81f8845f714b326407b3bec0c210d670a0104
Brian Mitchell <brianmitchell6@gmail.com>
2024-01-18 21:36:35 -0500

    Centered popup

--- Code/app.js
+++ Code/app.js
@@ -5,7 +5,7 @@
 //  //   shadowSize:   [50, 64], // size of the shadow
      iconAnchor:   [0, 0], // point of the icon which will correspond to marker's location
 //  //   shadowAnchor: [4, 62],  // the same for the shadow
-     popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+     popupAnchor:  [10, 20] // point from which the popup should open relative to the iconAnchor
  });
 // var foundIcon = L.icon({
 //     iconUrl: 'Icons/found.png',
